672b1f3a479e310fa3c690552e2f3581f05c578d
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2022-08-21 17:36:21 -0300

    feature (Cliente): adiciona cálculo de média de rtt e desvio padrão

--- 04/src/TCP/cliente/Cliente.java
+++ 04/src/TCP/cliente/Cliente.java
@@ -6,8 +6,10 @@ import java.io.*;
 public class Cliente {
     public static void main(String args[]) throws IOException {
         int port = 28887;
-
+        double[] rtt = new double[10000];
+        double tempoInicio = 0, tempoFim = 0, soma = 0, media = 0, somaDesvioPadrao = 0;
         for (int i = 0; i < 10000; i++) {
+            tempoInicio = System.currentTimeMillis();
             Socket socket = new Socket("localhost", port);
             System.out.println("Getting data...");
 
@@ -16,6 +18,21 @@ public class Cliente {
 
             socket.close();
             input.close();
+            tempoFim = System.currentTimeMillis();
+            rtt[i] = tempoFim - tempoInicio;
         }
+        for (int i = 0; i < 10000; i++) {
+            soma += rtt[i];
+        }
+        media = soma / 10000;
+        System.out.println("Tempo médio de RTT: " + media + "ms");
+
+        for (int i = 0; i < 10000; i++) {
+            somaDesvioPadrao = somaDesvioPadrao + Math.pow((rtt[i] - soma), 2);
+
+        }
+        double mediaDP = somaDesvioPadrao / 10000;
+        System.out.printf("Desvio padrão: %.3f\n", Math.sqrt(mediaDP));
     }
+
 }
--- 04/src/UDP/cliente/Cliente.java
+++ 04/src/UDP/cliente/Cliente.java
@@ -9,9 +9,12 @@ public class Cliente {
     public static void main(String[] args) throws IOException {
         int porta = 28886;
         String endereço = "localhost";
+        double[] rtt = new double[10000];
+        double tempoInicio = 0, tempoFim = 0, soma = 0, media = 0, somaDesvioPadrao = 0;
 
         for (int i = 0; i < 10000; i++) {
-            System.out.println("Teste " + i);
+            tempoInicio = System.currentTimeMillis();
+            // System.out.println("Teste " + i);
 
             DatagramSocket clientSocket = new DatagramSocket();
             InetAddress ipServidor = InetAddress.getByName(endereço);
@@ -26,7 +29,22 @@ public class Cliente {
 
             receberPacote = new DatagramPacket(receberDados, receberDados.length);
             clientSocket.receive(receberPacote);
-            System.out.println(new String(receberPacote.getData()));
+            // System.out.println(new String(receberPacote.getData()));
+            tempoFim = System.currentTimeMillis();
+            rtt[i] = tempoFim - tempoInicio;
         }
+
+        for (int i = 0; i < 10000; i++) {
+            soma += rtt[i];
+        }
+        media = soma / 10000;
+        System.out.println("Tempo médio de RTT: " + media + "ms");
+
+        for (int i = 0; i < 10000; i++) {
+            somaDesvioPadrao = somaDesvioPadrao + Math.pow((rtt[i] - soma), 2);
+
+        }
+        double mediaDP = somaDesvioPadrao / 10000;
+        System.out.printf("Desvio padrão: %.3f\n", Math.sqrt(mediaDP));
     }
 }
