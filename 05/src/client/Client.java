cd532fdb44e2cc5ddf81a73d5671b462ddd425ac
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2022-09-03 09:05:44 -0300

    add: 10000 requisicoes por cliente

--- 05/src/client/Client.java
+++ 05/src/client/Client.java
@@ -12,9 +12,11 @@ public class Client {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry(8888);
-            Datetime stub = (Datetime) registry.lookup("Time now: ");
-            String response = stub.getDatetime();
-            System.out.println("Response: " + response);
+            for (int i = 0; i < 10000; i++) {
+                Datetime stub = (Datetime) registry.lookup("Time now: ");
+                String response = stub.getDatetime();
+                System.out.println("Response: " + response);
+            }
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
