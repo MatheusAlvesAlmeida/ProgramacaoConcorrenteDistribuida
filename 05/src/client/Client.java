fbd32c4f1d48583425ea90034e06d813db7f8a3b
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2022-09-03 15:46:25 -0300

    feature (Client): save rtt into text file

--- 05/src/client/Client.java
+++ 05/src/client/Client.java
@@ -1,5 +1,9 @@
 package client;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -10,12 +14,41 @@ public class Client {
     }
 
     public static void main(String[] args) {
+        double tempoInicio = 0, tempoFim = 0, tempoTotal = 0;
+        // Create a new text file
+        try {
+            File myObj = new File("RTTs.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         try {
             Registry registry = LocateRegistry.getRegistry(8888);
             for (int i = 0; i < 10000; i++) {
+                tempoInicio = System.currentTimeMillis();
                 Datetime stub = (Datetime) registry.lookup("Time now: ");
                 String response = stub.getDatetime();
                 System.out.println("Response: " + response);
+                tempoFim = System.currentTimeMillis();
+                tempoTotal = tempoFim - tempoInicio;
+                System.out.println("Tempo de RTT: " + tempoTotal + "ms");
+                try {
+                    FileWriter fw = new FileWriter("RTTs.txt", true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    bw.write("" + tempoTotal);
+                    bw.newLine();
+                    bw.close();
+                    // System.out.println("Successfully wrote to the file.");
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
             }
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
