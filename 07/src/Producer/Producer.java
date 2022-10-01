60a4d76db650d797fd5d52a83928cd84aeee8625
Thayna Santos <tecs@cin.ufpe.br>
2022-09-30 23:01:51 -0300

    Sending system date and time

--- 07/src/Producer/Producer.java
+++ 07/src/Producer/Producer.java
@@ -1,5 +1,8 @@
 package Producer;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;   
@@ -7,6 +10,12 @@ import com.rabbitmq.client.ConnectionFactory;
 
 public class Producer {
     private static final String QUEUE_NAME = "queue";
+
+    public static String getDatetime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
 
     public static void main(String[] argv) throws Exception {
 
@@ -19,7 +28,7 @@ public class Producer {
                 channel.queueDeclare(QUEUE_NAME, false, false, false, null);
                 
                 for(int i = 0; i < 10000; i++){
-                    String message = "Client " + argv[0] + " message number " + Integer.toString(i);
+                    String message = "Client " + argv[0] + " message number " + Integer.toString(i) + ": [" + Producer.getDatetime() + "]";
                     channel.basicPublish("", QUEUE_NAME, null, message.getBytes()); // uses a default exchange, identified by the empty string ("")
                     System.out.println(" [x] Sent '" + message + "'");
                 }                
