9af638edd648f1744f14f97d72846ed36279f465
Thayna Santos <tecs@cin.ufpe.br>
2022-09-30 23:07:37 -0300

    Producers now send system date and time

--- 07/src/Producer/Producer.java
+++ 07/src/Producer/Producer.java
@@ -28,7 +28,7 @@ public class Producer {
                 channel.queueDeclare(QUEUE_NAME, false, false, false, null);
                 
                 for(int i = 0; i < 10000; i++){
-                    String message = "Client " + argv[0] + " message number " + Integer.toString(i) + ": [" + Producer.getDatetime() + "]";
+                    String message = "Producer " + argv[0] + " message number " + Integer.toString(i) + ": [" + Producer.getDatetime() + "]";
                     channel.basicPublish("", QUEUE_NAME, null, message.getBytes()); // uses a default exchange, identified by the empty string ("")
                     System.out.println(" [x] Sent '" + message + "'");
                 }                
