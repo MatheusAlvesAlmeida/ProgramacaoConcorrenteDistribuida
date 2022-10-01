5e687647505951987a80c4ff982161ccb9909002
Thayna Santos <tecs@cin.ufpe.br>
2022-09-30 22:50:34 -0300

    Producer + consumer + scripts + readme. Missing metrics.

---
+++ 07/src/Producer/Producer.java
@@ -0,0 +1,28 @@
+package Producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;   
+
+
+public class Producer {
+    private static final String QUEUE_NAME = "queue";
+
+    public static void main(String[] argv) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory(); 
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection(); // abstracts the socket connection
+             Channel channel = connection.createChannel()) {
+
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                
+                for(int i = 0; i < 10000; i++){
+                    String message = "Client " + argv[0] + " message number " + Integer.toString(i);
+                    channel.basicPublish("", QUEUE_NAME, null, message.getBytes()); // uses a default exchange, identified by the empty string ("")
+                    System.out.println(" [x] Sent '" + message + "'");
+                }                
+        }
+    }
+}
---
+++ 07/src/executeConsumer.sh
@@ -0,0 +1,9 @@
+wget https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/5.7.1/amqp-client-5.7.1.jar
+wget https://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.26/slf4j-api-1.7.26.jar
+wget https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/1.7.26/slf4j-simple-1.7.26.jar
+
+export CP=.:amqp-client-5.7.1.jar:slf4j-api-1.7.26.jar:slf4j-simple-1.7.26.jar
+
+javac -cp amqp-client-5.7.1.jar Consumer/Consumer.java Producer/Producer.java
+
+java -cp $CP Consumer/Consumer
