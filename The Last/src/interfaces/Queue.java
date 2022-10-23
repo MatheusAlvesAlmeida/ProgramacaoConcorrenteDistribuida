6a3052c16b54e5d865f8b3defcbca12b9dfef3db
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2022-10-23 19:56:25 -0300

    remove: queue file

--- The Last/src/interfaces/Queue.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Queue extends Remote {
-  Boolean isEmpty() throws RemoteException;
-  void printQueue() throws RemoteException;
-}
